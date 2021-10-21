ac515f5e722d591db0e925fe69b90232fa2b0c34
Rakhee <rakheegupta@users.noreply.github.com>
2021-10-21 09:22:06 -0700

    add layout for search activity

---
+++ app/src/main/java/com/example/whattoread/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.whattoread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       // https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20160112&sort=oldest&fq=news_desk:(%22Education%22%20%22Health%22)&api-key=VSqRE2UoTOiWA989w3qZzn0cki6TptBv
+    }
+}
