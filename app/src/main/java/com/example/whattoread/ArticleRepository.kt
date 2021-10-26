63d114a0923baf33cedad1ece9f961bc7a80ee3c
Rakhee <rakheegupta@users.noreply.github.com>
2021-10-26 11:02:47 -0700

    Add service and repository for api call

--- app/src/main/java/com/example/whattoread/ArticleRepository.kt
+++ app/src/main/java/com/example/whattoread/ArticleRepository.kt
@@ -1,21 +1,45 @@
 package com.example.whattoread
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ArticleRepository {
     val mArticles = MutableLiveData<List<Article>>()
-
-    init{
-        fetchArticles()
-    }
 
     fun getArticles():LiveData<List<Article>>{
         return mArticles
     }
 
-    fun fetchArticles()
+    fun fetchArticles(sortOrder:String,beginDate:String,funcQueryParams:String)
     {
+        val apiService = ArticleService()
+        val call: Call<NYTApiResponse> = apiService.getArticlesFromNYT(sortOrder,beginDate,funcQueryParams)
+
+        call.enqueue(object : Callback<NYTApiResponse>
+        {
+            override fun onResponse(
+                call: Call<NYTApiResponse>,
+                response: Response<NYTApiResponse>
+            ) {
+                val apiResponse: NYTApiResponse? = response.body()
+                if(apiResponse != null){
+                    mArticles.value = apiResponse.results
+                    Log.i("API", "onResponse: ${apiResponse.results.size}")
+                }
+                else
+                    Log.i("API","No Results")
+                if(response.errorBody() != null)
+                    Log.i("API","error body: ${response.errorBody()}")
+            }
+
+            override fun onFailure(call: Call<NYTApiResponse>, t: Throwable) {
+                Log.i("NYT API: ","Faliure")
+            }
+        })
 
     }
 }
--- app/src/main/java/com/example/whattoread/ArticleService.kt
+++ app/src/main/java/com/example/whattoread/ArticleService.kt
@@ -1,5 +1,16 @@
 package com.example.whattoread
 
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
 //https://api.nytimes.com/svc/search/v2/
 // articlesearch.json?
 // begin_date=20160112&
@@ -8,8 +19,41 @@ package com.example.whattoread
 //  api-key=VSqRE2UoTOiWA989w3qZzn0cki6TptBv
 
 const val API_KEY:String = "VSqRE2UoTOiWA989w3qZzn0cki6TptBv"
+const val BASE_URL:String ="https://api.nytimes.com/svc/search/v2/"
 
-interface ArticleService {
+interface ArticleService{
 
+    @GET("articlesearch.json")
+    fun getArticlesFromNYT(@Query("sort") sortOrder:String,
+                           @Query("begin_date") beginDate:String,
+                           @Query("fq") funcQueryParams:String)
+    : Call<NYTApiResponse>
 
+    companion object {
+        operator fun invoke():ArticleService{
+            val requestInterceptor = Interceptor{chain->
+                val url:HttpUrl = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("api-key", API_KEY)
+                    .build()
+                val newRequest: Request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+                return@Interceptor chain.proceed(newRequest)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(client)
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ArticleService::class.java)
+        }
+    }
 }
